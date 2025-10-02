6199dea472466769dedc28e47b6eefcbb7fd51ed
Alessio Stalla <alessiostalla@gmail.com>
2025-10-02 18:23:04 +0200

    Optimize JavaNode reflection

--- core/src/main/kotlin/com/strumenta/kolasu/testing/Testing.kt
+++ core/src/main/kotlin/com/strumenta/kolasu/testing/Testing.kt
@@ -163,129 +163,123 @@ fun assertASTsAreEqual(
     considerPosition: Boolean = false,
     useLightweightAttributeEquality: Boolean = false
 ) {
-    if (expected.nodeType == actual.nodeType) {
-        if (considerPosition) {
-            assertEquals(expected.position, actual.position, "$context.position")
-        }
-        expected.originalProperties.forEach { expectedProperty ->
-            try {
-                val actualProperty = actual.originalProperties.find { it.name == expectedProperty.name }
-                    ?: fail("No property ${expectedProperty.name} found at $context")
-                val actualPropValue = actualProperty.value
-                val expectedPropValue = expectedProperty.value
-                if (expectedProperty.provideNodes) {
-                    if (expectedProperty.multiple) {
-                        if (expectedPropValue is IgnoreChildren<*>) {
-                            // Nothing to do
-                        } else {
-                            val actualPropValueCollection = actualPropValue?.let { it as Collection<Node> }
-                            val expectedPropValueCollection = expectedPropValue?.let { it as Collection<Node> }
+    assertEquals(expected.nodeType, actual.nodeType,"$context: node types are different")
+    if (considerPosition) {
+        assertEquals(expected.position, actual.position, "$context.position")
+    }
+    expected.originalProperties.forEach { expectedProperty ->
+        try {
+            val actualProperty = actual.originalProperties.find { it.name == expectedProperty.name }
+                ?: fail("No property ${expectedProperty.name} found at $context")
+            val actualPropValue = actualProperty.value
+            val expectedPropValue = expectedProperty.value
+            if (expectedProperty.provideNodes) {
+                if (expectedProperty.multiple) {
+                    if (expectedPropValue is IgnoreChildren<*>) {
+                        // Nothing to do
+                    } else {
+                        val actualPropValueCollection = actualPropValue?.let { it as Collection<Node> }
+                        val expectedPropValueCollection = expectedPropValue?.let { it as Collection<Node> }
+                        assertEquals(
+                            actualPropValueCollection == null,
+                            expectedPropValueCollection == null,
+                            "$context.${expectedProperty.name} nullness: expected value " +
+                                "to be $expectedPropValueCollection but was $actualPropValueCollection " +
+                                "(node type ${actual.nodeType})"
+                        )
+                        if (actualPropValueCollection != null && expectedPropValueCollection != null) {
                             assertEquals(
-                                actualPropValueCollection == null,
-                                expectedPropValueCollection == null,
-                                "$context.${expectedProperty.name} nullness: expected value " +
-                                    "to be $expectedPropValueCollection but was $actualPropValueCollection " +
-                                    "(node type ${actual.nodeType})"
+                                expectedPropValueCollection?.size,
+                                actualPropValueCollection?.size,
+                                "$context.${expectedProperty.name} length"
                             )
-                            if (actualPropValueCollection != null && expectedPropValueCollection != null) {
-                                assertEquals(
-                                    expectedPropValueCollection?.size,
-                                    actualPropValueCollection?.size,
-                                    "$context.${expectedProperty.name} length"
+                            val expectedIt = expectedPropValueCollection.iterator()
+                            val actualIt = actualPropValueCollection.iterator()
+                            for (i in expectedPropValueCollection.indices) {
+                                assertASTsAreEqual(
+                                    expectedIt.next(),
+                                    actualIt.next(),
+                                    "$context.${expectedProperty.name}[$i]",
+                                    considerPosition = considerPosition,
+                                    useLightweightAttributeEquality = useLightweightAttributeEquality
                                 )
-                                val expectedIt = expectedPropValueCollection.iterator()
-                                val actualIt = actualPropValueCollection.iterator()
-                                for (i in expectedPropValueCollection.indices) {
-                                    assertASTsAreEqual(
-                                        expectedIt.next(),
-                                        actualIt.next(),
-                                        "$context.${expectedProperty.name}[$i]",
-                                        considerPosition = considerPosition,
-                                        useLightweightAttributeEquality = useLightweightAttributeEquality
-                                    )
-                                }
                             }
                         }
-                    } else {
-                        if (expectedPropValue == null && actualPropValue != null) {
-                            assertEquals<Any?>(
-                                expectedPropValue,
-                                actualPropValue,
-                                "$context.${expectedProperty.name}"
-                            )
-                        } else if (expectedPropValue != null && actualPropValue == null) {
-                            assertEquals<Any?>(
-                                expectedPropValue,
-                                actualPropValue,
-                                "$context.${expectedProperty.name}"
-                            )
-                        } else if (expectedPropValue == null && actualPropValue == null) {
-                            // that is ok
-                        } else {
-                            assertASTsAreEqual(
-                                expectedPropValue as Node,
-                                actualPropValue as Node,
-                                context = "$context.${expectedProperty.name}",
-                                considerPosition = considerPosition,
-                                useLightweightAttributeEquality = useLightweightAttributeEquality
-                            )
-                        }
-                    }
-                } else if (expectedProperty.propertyType == PropertyType.REFERENCE) {
-                    if (expectedPropValue is ReferenceByName<*> && actualPropValue is ReferenceByName<*>) {
-                        assertEquals(
-                            expectedPropValue.name,
-                            actualPropValue.name,
-                            "$context, comparing reference name of ${expectedProperty.name} of ${expected.nodeType}"
-                        )
-                        assertEquals(
-                            expectedPropValue.referred?.toString(),
-                            actualPropValue.referred?.toString(),
-                            "$context, comparing reference pointer ${expectedProperty.name} of ${expected.nodeType}"
-                        )
-                    } else {
-                        if (expectedPropValue != null) {
-                            assertIs<ReferenceByName<*>>(
-                                expectedPropValue,
-                                "$context, checking reference ${expectedProperty.name} of ${expected.nodeType}"
-                            )
-                        }
-                        if (actualPropValue != null) {
-                            assertIs<ReferenceByName<*>>(
-                                actualPropValue,
-                                "$context, checking reference ${actualProperty.name} of ${actual.nodeType}"
-                            )
-                        }
-                        assertEquals(
-                            expectedPropValue,
-                            actualPropValue,
-                            "$context, comparing reference ${expectedProperty.name} of ${expected.nodeType}"
-                        )
                     }
                 } else {
-                    if (useLightweightAttributeEquality) {
-                        assertEquals(
-                            expectedPropValue?.toString(),
-                            actualPropValue?.toString(),
-                            "$context, comparing property ${expectedProperty.name} of ${expected.nodeType}"
-                        )
-                    } else {
-                        assertEquals(
+                    if (expectedPropValue == null && actualPropValue != null) {
+                        assertEquals<Any?>(
                             expectedPropValue,
                             actualPropValue,
-                            "$context, comparing property ${expectedProperty.name} of ${expected.nodeType}"
+                            "$context.${expectedProperty.name}"
+                        )
+                    } else if (expectedPropValue != null && actualPropValue == null) {
+                        assertEquals<Any?>(
+                            expectedPropValue,
+                            actualPropValue,
+                            "$context.${expectedProperty.name}"
+                        )
+                    } else if (expectedPropValue == null && actualPropValue == null) {
+                        // that is ok
+                    } else {
+                        assertASTsAreEqual(
+                            expectedPropValue as Node,
+                            actualPropValue as Node,
+                            context = "$context.${expectedProperty.name}",
+                            considerPosition = considerPosition,
+                            useLightweightAttributeEquality = useLightweightAttributeEquality
                         )
                     }
                 }
-            } catch (e: Exception) {
-                throw RuntimeException("Issue while processing property $expectedProperty of $expected", e)
+            } else if (expectedProperty.propertyType == PropertyType.REFERENCE) {
+                if (expectedPropValue is ReferenceByName<*> && actualPropValue is ReferenceByName<*>) {
+                    assertEquals(
+                        expectedPropValue.name,
+                        actualPropValue.name,
+                        "$context, comparing reference name of ${expectedProperty.name} of ${expected.nodeType}"
+                    )
+                    assertEquals(
+                        expectedPropValue.referred?.toString(),
+                        actualPropValue.referred?.toString(),
+                        "$context, comparing reference pointer ${expectedProperty.name} of ${expected.nodeType}"
+                    )
+                } else {
+                    if (expectedPropValue != null) {
+                        assertIs<ReferenceByName<*>>(
+                            expectedPropValue,
+                            "$context, checking reference ${expectedProperty.name} of ${expected.nodeType}"
+                        )
+                    }
+                    if (actualPropValue != null) {
+                        assertIs<ReferenceByName<*>>(
+                            actualPropValue,
+                            "$context, checking reference ${actualProperty.name} of ${actual.nodeType}"
+                        )
+                    }
+                    assertEquals(
+                        expectedPropValue,
+                        actualPropValue,
+                        "$context, comparing reference ${expectedProperty.name} of ${expected.nodeType}"
+                    )
+                }
+            } else {
+                if (useLightweightAttributeEquality) {
+                    assertEquals(
+                        expectedPropValue?.toString(),
+                        actualPropValue?.toString(),
+                        "$context, comparing property ${expectedProperty.name} of ${expected.nodeType}"
+                    )
+                } else {
+                    assertEquals(
+                        expectedPropValue,
+                        actualPropValue,
+                        "$context, comparing property ${expectedProperty.name} of ${expected.nodeType}"
+                    )
+                }
             }
+        } catch (e: Exception) {
+            throw RuntimeException("Issue while processing property $expectedProperty of $expected", e)
         }
-    } else {
-        fail(
-            "$context: expected node of type ${expected.nodeType}, " +
-                "but found ${actual.nodeType}"
-        )
     }
 }
 
