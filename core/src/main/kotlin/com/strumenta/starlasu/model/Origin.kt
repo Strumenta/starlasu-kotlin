ad2ca98451983402ac370b52dd2dcc2b0af3a9a1
ftomassetti <federico@strumenta.com>
2025-10-15 15:34:44 +0200

    Working on using ASTNode instead of BaseASTNode

--- core/src/main/kotlin/com/strumenta/starlasu/model/Origin.kt
+++ core/src/main/kotlin/com/strumenta/starlasu/model/Origin.kt
@@ -3,8 +3,13 @@ package com.strumenta.starlasu.model
 import java.io.Serializable
 
 interface Origin {
+    @Internal
     val position: Position?
+
+    @Internal
     val sourceText: String?
+
+    @Internal
     val source: Source?
         get() = position?.source
 }
