952fc76f44d9b5d19d57a0e22b4021eb4d5340c2
ftomassetti <federico@strumenta.com>
2025-10-18 12:27:51 +0200

    Set up Maven Central automatic release and enrich POM metadata for all modules

--- codebase/build.gradle.kts
+++ codebase/build.gradle.kts
@@ -22,3 +22,39 @@ tasks.named("dokkaJavadoc").configure {
     dependsOn(":core:compileKotlin")
     dependsOn(":lionweb:jar")
 }
+
+mavenPublishing {
+    publishToMavenCentral(automaticRelease = true)
+    coordinates(project.group as String, "starlasu-${project.name}", project.version as String)
+
+    pom {
+        name.set(project.name)
+        description.set("A framework for Language Engineering")
+        inceptionYear.set("2017")
+        url.set("https://github.com/Strumenta/starlasu-kotlin/")
+        licenses {
+            license {
+                name.set("The Apache License, Version 2.0")
+                url.set("http://www.apache.org/licenses/LICENSE-2.0.txt")
+                distribution.set("http://www.apache.org/licenses/LICENSE-2.0.txt")
+            }
+        }
+        developers {
+            developer {
+                id.set("ftomassetti")
+                name.set("Federico Tomassetti")
+                email.set("federico@strumenta.com")
+            }
+            developer {
+                id.set("alessiostalla")
+                name.set("Alessio Stalla")
+                email.set("alessio.stalla@strumenta.com")
+            }
+        }
+        scm {
+            url.set("https://github.com/Strumenta/starlasu-kotlin/")
+            connection.set("scm:git:git://github.com/Strumenta/starlasu-kotlin.git")
+            developerConnection.set("scm:git:ssh://git@github.com/Strumenta/starlasu-kotlin.git")
+        }
+    }
+}
--- lionweb/build.gradle.kts
+++ lionweb/build.gradle.kts
@@ -28,9 +28,38 @@ dependencies {
 }
 
 mavenPublishing {
-    coordinates(
-        project.group.toString(),
-        "starlasu-kotlin-${project.name}",
-        project.version.toString(),
-    )
+    publishToMavenCentral(automaticRelease = true)
+    coordinates(project.group.toString(),
+        "starlasu-${project.name}", project.version.toString())
+
+    pom {
+        name.set(project.name)
+        description.set("A framework for Language Engineering")
+        inceptionYear.set("2017")
+        url.set("https://github.com/Strumenta/starlasu-kotlin/")
+        licenses {
+            license {
+                name.set("The Apache License, Version 2.0")
+                url.set("http://www.apache.org/licenses/LICENSE-2.0.txt")
+                distribution.set("http://www.apache.org/licenses/LICENSE-2.0.txt")
+            }
+        }
+        developers {
+            developer {
+                id.set("ftomassetti")
+                name.set("Federico Tomassetti")
+                email.set("federico@strumenta.com")
+            }
+            developer {
+                id.set("alessiostalla")
+                name.set("Alessio Stalla")
+                email.set("alessio.stalla@strumenta.com")
+            }
+        }
+        scm {
+            url.set("https://github.com/Strumenta/starlasu-kotlin/")
+            connection.set("scm:git:git://github.com/Strumenta/starlasu-kotlin.git")
+            developerConnection.set("scm:git:ssh://git@github.com/Strumenta/starlasu-kotlin.git")
+        }
+    }
 }
