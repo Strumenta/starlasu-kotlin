ba8ce264abbde1526a0a38020af4eae22a7d8bbe
ftomassetti <federico@strumenta.com>
2025-10-18 10:30:18 +0200

    Update Maven publishing configuration with detailed POM metadata and adjusted coordinates

--- core/build.gradle.kts
+++ core/build.gradle.kts
@@ -80,9 +80,36 @@ project.afterEvaluate {
 }
 
 mavenPublishing {
-    coordinates(
-        project.group.toString(),
-        "starlasu-kotlin-${project.name}",
-        project.version.toString(),
-    )
+    coordinates("com.example.mylibrary", "mylibrary-runtime", "1.0.3-SNAPSHOT")
+
+    pom {
+        name.set(project.name)
+        description.set("A framework for Language Engineering")
+        inceptionYear.set("2017")
+        url.set("https://github.com/Strumenta/starlasu-kotlin/")
+        licenses {
+            license {
+                name.set("The Apache License, Version 2.0")
+                url.set("http://www.apache.org/licenses/LICENSE-2.0.txt")
+                distribution.set("http://www.apache.org/licenses/LICENSE-2.0.txt")
+            }
+        }
+        developers {
+            developer {
+                id.set("ftomassetti")
+                name.set("Federico Tomassetti")
+                email.set("federico@strumenta.com")
+            }
+            developer {
+                id.set("alessiostalla")
+                name.set("Alessio Stalla")
+                email.set("alessio.stalla@strumenta.com")
+            }
+        }
+        scm {
+            url.set("https://github.com/Strumenta/starlasu-kotlin/")
+            connection.set("scm:git:git://github.com/Strumenta/starlasu-kotlin.git")
+            developerConnection.set("scm:git:ssh://git@github.com/Strumenta/starlasu-kotlin.git")
+        }
+    }
 }
