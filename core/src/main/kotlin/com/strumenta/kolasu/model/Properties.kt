671bccaa7a026ed730283a5acfb9660569584fbc
Federico Tomassetti <federico@strumenta.com>
2026-03-03 09:09:14 +0100

    Introduce thread-safe caching for reflection metadata
    Add `ConcurrentHashMap`-based caching to `nodeProperties` and `PropertyMeta` to significantly reduce redundant reflection computations, improving performance and thread safety.

--- core/src/main/kotlin/com/strumenta/kolasu/model/Properties.kt
+++ core/src/main/kotlin/com/strumenta/kolasu/model/Properties.kt
@@ -12,18 +12,40 @@ val <T : Any> Class<T>.nodeOriginalProperties: Collection<KProperty1<T, *>>
     get() = this.kotlin.nodeOriginalProperties
 val <T : Any> Class<T>.nodeDerivedProperties: Collection<KProperty1<T, *>>
     get() = this.kotlin.nodeDerivedProperties
+//val <T : Any> KClass<T>.nodeProperties: Collection<KProperty1<T, *>>
+//    get() = memberProperties.asSequence()
+//        .filter { it.visibility == KVisibility.PUBLIC }
+//        .filter { it.findAnnotation<Internal>() == null }
+//        .filter { it.findAnnotation<Link>() == null }
+//        .map {
+//            require(it.name !in RESERVED_FEATURE_NAMES) {
+//                "Property ${it.name} in ${this.qualifiedName} should be marked as internal"
+//            }
+//            it
+//        }
+//        .toList()
+
+
+// 1. Define a thread-safe static cache for class properties
+private val nodePropertiesCache = java.util.concurrent.ConcurrentHashMap<kotlin.reflect.KClass<*>, Collection<kotlin.reflect.KProperty1<*, *>>>()
+
+// 2. Update the extension property to use the cache
+@Suppress("UNCHECKED_CAST")
 val <T : Any> KClass<T>.nodeProperties: Collection<KProperty1<T, *>>
-    get() = memberProperties.asSequence()
-        .filter { it.visibility == KVisibility.PUBLIC }
-        .filter { it.findAnnotation<Internal>() == null }
-        .filter { it.findAnnotation<Link>() == null }
-        .map {
-            require(it.name !in RESERVED_FEATURE_NAMES) {
-                "Property ${it.name} in ${this.qualifiedName} should be marked as internal"
+    get() = nodePropertiesCache.computeIfAbsent(this) { kClass ->
+        // This heavy reflection block will now run EXACTLY ONCE per class type
+        kClass.memberProperties.asSequence()
+            .filter { it.visibility == kotlin.reflect.KVisibility.PUBLIC }
+            .filter { it.findAnnotation<Internal>() == null }
+            .filter { it.findAnnotation<Link>() == null }
+            .map {
+                require(it.name !in RESERVED_FEATURE_NAMES) {
+                    "Property ${it.name} in ${kClass.qualifiedName} should be marked as internal"
+                }
+                it
             }
-            it
-        }
-        .toList()
+            .toList()
+    } as Collection<KProperty1<T, *>>
 
 val <T : Any> KClass<T>.nodeOriginalProperties: Collection<KProperty1<T, *>>
     get() = nodeProperties
