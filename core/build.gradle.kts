98310e54a816dd23a241bc32bbc69b8dd93f6572
Federico Tomassetti <federico@strumenta.com>
2026-04-14 16:59:35 +0200

    Change `test-junit` configuration from `api` to `implementation` in build script.

--- core/build.gradle.kts
+++ core/build.gradle.kts
@@ -26,7 +26,7 @@ dependencies {
     implementation(libs.starlasu.specs)
     implementation(kotlin("stdlib", libs.versions.kotlin.get()))
     implementation(kotlin("reflect", libs.versions.kotlin.get()))
-    api(kotlin("test-junit", libs.versions.kotlin.get()))
+    implementation(kotlin("test-junit", libs.versions.kotlin.get()))
     implementation(libs.gson)
     // To be removed in v1.7
     implementation("org.redundent:kotlin-xml-builder:1.7.3")
